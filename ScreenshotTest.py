b5f8fe194b231432f542a818b0f1c3cd3e7e0c63
Ozzius <deb991@users.noreply.github.com>
2017-09-07 10:27:08 +0530

    <<New Development>>
    Modules for New development & reuse codes for Mature framework.

---
+++ ScreenshotTest.py
@@ -0,0 +1,6 @@
+import pyscreenshot as ImageGrab
+# fullscreen
+if __name__ == "__main__":
+    im=ImageGrab.grab()
+    im.show()
+#
---
+++ screenshotTest.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Python27\python.exe C:\Users\%username%\Documents\MATURE_Framework\PyGUI\ScreenshotTest.py
